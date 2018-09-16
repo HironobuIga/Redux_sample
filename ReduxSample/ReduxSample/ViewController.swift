e7d68f644f4651539529449bcff43dc646d0b0d6
iganin <hironobu.iga.developer@gmail.com>
2018-09-16 20:10:06 +0900

    Redux サンプルプロジェクトの作成

---
+++ ReduxSample/ReduxSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReduxSample
+//
+//  Created by 伊賀裕展 on 2018/09/16.
+//  Copyright © 2018年 iganin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
