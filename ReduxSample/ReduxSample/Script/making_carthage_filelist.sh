df861bf71a21eb6e0f4e30893b42c3bb9d627662
iganin <hironobu.iga.developer@gmail.com>
2018-11-09 23:40:31 +0900

    微修正

--- ReduxSample/ReduxSample/Script/making_carthage_filelist.sh
+++ ReduxSample/ReduxSample/Script/making_carthage_filelist.sh
@@ -7,6 +7,5 @@
 #  Copyright © 2018年 iganin. All rights reserved.
 
 GitRoot=$(git rev-parse --show-toplevel)
-
 ls $GitRoot/ReduxSample/Carthage/Build/iOS | grep -e .framework$ | sed 's/.*/$(SRCROOT)\/Carthage\/Build\/iOS\/&/' > $GitRoot/ReduxSample/ReduxSample/Support/CarthageInput.xcfilelist
 ls $GitRoot/ReduxSample/Carthage/Build/iOS | grep -e .framework$ | sed 's/.*/$(BUILT_PRODUCTS_DIR)\/$(FRAMEWORKS_FOLDER_PATH)\/&/' > $GitRoot/ReduxSample/ReduxSample/Support/CarthageOutput.xcfilelist
